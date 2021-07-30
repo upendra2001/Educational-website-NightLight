18d27e758b7c7b1ae3fa4c1193ddafc3836fb058
Vaibhav <vkpstudyy247@gmail.com>
2021-07-30 23:49:54 +0530

    made artile page

--- src/components/articles/article.js
+++ src/components/articles/article.js
@@ -1,1 +1,46 @@
-rfc
+import React, { useState } from 'react'
+import "../../css/article/article.css"
+import img1 from "../../assets/photo-1581447109217-19026003eba5_adobespark (1).jfif"
+
+const content1 = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga esse debitis suscipit ducimus distinctio iusto, quod provident cum earum necessitatibus voluptates veniam cupiditate ratione ullam iste quam impedit. Voluptatum dolor, doloremque delectus corporis eaque totam quo possimus deserunt facere officia veniam autem molestiae amet eum nihil, expedita numquam rerum sed ipsam porro maiores nisi cumque suscipit! Dolor exercitationem nihil fugiat! Ducimus ipsum iste nostrum voluptatum tempore libero ut repellat maiores possimus consequatur provident sed veritatis cumque excepturi eaque doloribus iure officia quo, atque nobis fuga nulla. Harum tenetur nostrum nesciunt, pariatur dicta atque dolor et, ea minus quos obcaecati sunt aspernatur sapiente odit autem odio numquam? Saepe, eligendi nisi quas, at exercitationem error optio a delectus distinctio accusantium nostrum ipsam quasi perspiciatis? Quibusdam vitae dicta ratione nulla beatae sapiente? Blanditiis eum temporibus dolore sunt. Magni dolorem voluptates sed repellat quia dolorum veritatis, blanditiis dolore rerum possimus amet voluptate quo laborum reiciendis ex, repellendus nam numquam ad labore. Perspiciatis ratione nulla ex earum architecto voluptas sapiente quo fugiat. Commodi repudiandae eum nemo non, rerum voluptatum eveniet velit, rem accusantium blanditiis explicabo, a accusamus odio soluta quod? Impedit possimus ut odio et ipsa sequi mollitia eveniet rerum, dolorem tempore eaque, harum veritatis?"
+const content2 = "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempora quaerat nulla quidem, at ducimus cupiditate, ad aspernatur, quibusdam ipsam ut ullam minus a quod quo deserunt nesciunt deleniti. Sequi neque est molestias odio expedita dignissimos eveniet inventore sapiente laborum vel et earum labore aspernatur officiis, molestiae nostrum consequuntur asperiores, modi tempore repellendus temporibus veritatis explicabo! Laborum in cum nobis omnis exercitationem id sapiente animi velit ullam consequatur! Harum eum inventore pariatur corrupti explicabo repellat at. Cupiditate modi iure expedita quasi doloribus quo accusamus atque excepturi, iusto laboriosam officiis quisquam? Qui perferendis maiores repellat distinctio? Quasi asperiores, repellat commodi provident libero quas, exercitationem accusantium earum dolores facilis dolorum sit consectetur tempore sequi quisquam. Neque iure mollitia suscipit accusantium unde, obcaecati perspiciatis."
+export default function Article() {
+    const [likes, setLikes] = useState(15)
+    const [likeToggler, setLikeToggler] = useState(0)
+    return (
+        <div className="article-container">
+            <div className="article-main-container">
+                <div className="article-title">
+                    <h1>Article Title</h1>
+                    <small>Created: 24-May-2021</small>
+                </div>
+                <div className="content-container">
+                    <p className="content1">{content1} </p>
+                    <img className="img1" src={img1} alt="..." />
+                    <p>{content2}</p>
+                </div>
+                <div className="article-buttons">
+                    <button className="article-buttons-previous">Previous</button>
+                    <button onClick={(e) => {
+                        e.preventDefault()
+                        if (likeToggler === 0) {setLikeToggler(1);setLikes(likes+1);}
+                        else {setLikeToggler(0);setLikes(likes-1)} 
+                    }}>
+
+                        <small>{likes}</small> Liked It!!
+                        {
+                            likeToggler ?
+                                <i className="fas fa-heart"></i> :
+                                <i className="far fa-heart"></i>
+                        }
+                    </button>
+
+
+                    <button className="article-buttons-next">Next</button>
+
+                </div>
+
+            </div>
+        </div >
+    )
+}
--- src/components/home/Collections.js
+++ src/components/home/Collections.js
@@ -3,36 +3,37 @@ import '../../css/home/collections.css'
 import resume from '../../assets/resume-copy-on-work-place-260nw-1434400523.jpg'
 import faqs from '../../assets/FAQ_0130.jpg'
 import quote from '../../assets/Bhagavad-Gita-Quotes-1.jpg'
+import { Link } from 'react-router-dom'
 
 export default function Collections() {
     return (
         <div className="collection-container">
             <div className="main-container">
                 <div className="container-col col-left">
-                    <div className="col-items col-left-top">
+                    <Link to="/articles"><div className="col-items col-left-top">
                         <p>Article</p>
-                    </div>
-                    <div className="col-items col-left-mid col-left-mid-bottom">
+                    </div></Link>
+                    <Link><div className="col-items col-left-mid col-left-mid-bottom">
                         <p>Quizzes/MCQs</p>
-                    </div>
-                    <div className="col-items col-left-bottom col-left-mid-bottom"> 
+                    </div></Link>
+                    <Link><div className="col-items col-left-bottom col-left-mid-bottom">
                         <p>Resume</p>
-                    </div>
+                    </div></Link>
                 </div>
                 <div className="container-col col-mid">
                     <p> QUOTES / MOTIVATIONAL THOUGHTS / THOUGHT OF THE DAY Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, rem explicabo corporis quisquam dolorum saepe, voluptatem facilis distinctio deserunt cumque totam, voluptas quidem unde. Atque laborum repudiandae accusantium expedita eum, nostrum veniam nesciunt quidem.</p>
                     {/* <img src={quote} alt="..." /> */}
                 </div>
                 <div className="container-col col-right">
-                    <div className="col-items col-right-top col-right-top-mid">
+                    <Link><div className="col-items col-right-top col-right-top-mid">
                         <p>Notes</p>
-                    </div>
-                    <div className="col-items col-right-mid col-right-top-mid">
+                    </div></Link>
+                    <Link><div className="col-items col-right-mid col-right-top-mid">
                         <p>Video Links</p>
-                    </div>
-                    <div className="col-items col-right-bottom">
+                    </div></Link>
+                    <Link><div className="col-items col-right-bottom">
                         <p>FAQs!!</p>
-                    </div>
+                    </div></Link>
                 </div>
             </div>
         </div>
